d9c378a1d68a427ca3bc5219ca1bc0ec56ef49a1
Armael <armael.gueneau@ens-lyon.fr>
2015-06-15 08:42:39 +0200

    Finaliser for Image.t

--- image.ml
+++ image.ml
@@ -5,7 +5,13 @@ open Operators
 
 type t = { surface: Sdl.surface; texture: Sdl.texture }
 
+let free_t t =
+  Sdl.free_surface t.surface;
+  Sdl.destroy_texture t.texture
+
 let load t path =
   Sdl.load_bmp path >>= fun surface ->
   Sdl.create_texture_from_surface t.renderer surface >>= fun texture ->
-  return { surface; texture }
+  let t = { surface; texture } in
+  Gc.finalise free_t t;
+  return t
