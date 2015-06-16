a6ca248f703b482ed27afa9a93be4865a128a1bd
Armael <armael@isomorphis.me>
2015-06-16 09:34:32 +0200

    A variant of React.E.map that allows for an initialization value to be created
    An initial value is created from the first tick of the event, and given to the
    mapped function for every tick of the event.

--- prelude.ml
+++ prelude.ml
@@ -8,3 +8,13 @@ module Int32 = struct
   let max a b = if a < b then b else a
   let min a b = if a < b then a else b
 end
+
+let event_map_init
+    (init: 'a -> 'b)
+    (f: 'a -> 'b -> 'c)
+    (e: 'a React.E.t):
+  'c React.E.t
+  =
+  React.E.map init (React.E.once e)
+  |> React.E.map (fun init_value -> React.E.map (fun x -> f x init_value) e)
+  |> React.E.switch React.E.never
